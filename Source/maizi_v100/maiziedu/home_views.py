96d97b4872c80d68c57a49bc1e9bbc69dbc1407c
huang-t <huangtao0202@gmail.com>
2015-08-15 18:18:13 +0800

    添加base.html

--- Source/maizi_v100/maiziedu/home_views.py
+++ Source/maizi_v100/maiziedu/home_views.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-# Create your views here.
+# 首页
 def index(request):
-    return HttpResponse('<center><h1><cneter>欢迎来到麦子官网1.0项目，下面，让我们开始愉快地干活吧^_^</h1></center>')
+    return render(request, 'base.html', locals())
