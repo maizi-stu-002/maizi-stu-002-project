1869c8dea51dcfa7a9ed32a1c45ff400fde56094
huang-t <huangtao0202@gmail.com>
2015-08-19 22:36:35 +0800

    完成首页课程模块。

--- Source/maizi_v100/maiziedu/home_views.py
+++ Source/maizi_v100/maiziedu/home_views.py
@@ -34,19 +34,27 @@ def index(request):
         weixin = settings.WEIXIN  # 官方微信
         # 课程
         new_add = Course.objects.order_by('-date_publish')  # 最新课程
+        # 最新课程分页
+        new_add = get_page(request, new_add)
+
         most_play = Course.objects.order_by('-play_count')  # 最多播放
+        # 最多播放分页
+        most_play = get_page(request, most_play)
+
         hot_favorite = Course.objects.order_by('-favorite_count')  # 最具人气
+        # 最具人气分页
+        hot_favorite = get_page(request, hot_favorite)
         return render(request, 'home/index.html', locals())
     except Exception as e:
         logger.error(e)
 
 
 # 分页代码
-def get_page(request, article_list):
-    paginator = Paginator(article_list, 8)  # 一页最多显示8们课程
+def get_page(request, course_list):
+    paginator = Paginator(course_list, 8)  # 一页最多显示8门课程
     try:
         page = int(request.GET.get('page', 1))
-        article_list = paginator.page(page)
+        course_list = paginator.page(page)
     except (EmptyPage, InvalidPage, PageNotAnInteger):
-        article_list = paginator.page(1)  # 出现异常就返回第一页
-    return article_list
+        course_list = paginator.page(1)  # 出现异常就返回第一页
+    return course_list
